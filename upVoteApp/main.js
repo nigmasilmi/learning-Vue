0eab76221e7f9aa51509826f59b5246ef037ad48
nigmasilmi <nigmasilmi.web@gmail.com>
2020-08-02 15:05:36 -0400

    upVote app functional

---
+++ upVoteApp/main.js
@@ -0,0 +1,52 @@
+const submissionsData = [
+    {
+        id: 1,
+        title: 'Lorem Laudatium',
+        description: 'Laudantium enim quasi es…am sapiente accusantium',
+        url: '#',
+        votes: 16,
+        avatar: './images/freepik-avatar-02.jpg',
+        submissionImage: './images/7.png',
+    },
+    {
+        id: 2,
+        title: 'Reprehend Lorem',
+        description: 'Quia molestiae reprehend…epturi deleniti ratione',
+        url: '#',
+        votes: 12,
+        avatar: './images/freepik-avatar-03.jpg',
+        submissionImage: './images/softwareD.png',
+    },
+    {
+        id: 3,
+        title: 'Lorem Voluptates',
+        description: 'Nihil ut voluptates blan…estias tempora quo quia',
+        url: '#',
+        votes: 14,
+        avatar: './images/freepik-avatar-04.jpg',
+        submissionImage: './images/taskman.png',
+    }
+];
+
+new Vue({
+    el: '#app',
+    data: {
+        submissions: submissionsData
+    },
+    computed: {
+        sortedSubmissions() {
+            return this.submissions.sort((a, b) => {
+                return b.votes - a.votes;
+            });
+        }
+    },
+    methods: {
+        upVote(postid) {
+            const submissionFound = this.submissions.find(element => {
+                return element.id === postid;
+            });
+
+            submissionFound.votes++;
+        }
+    }
+});
---
+++ upVoteApp/seed.js
@@ -0,0 +1,30 @@
+
+const submissionsData = [
+    {
+        id: 1,
+        title: 'Lorem Laudatium',
+        description: 'Laudantium enim quasi es…am sapiente accusantium',
+        url: '#',
+        votes: 16,
+        avatar: './images/freepik-avatar-02.jpg',
+        submissionImage: './images/7.png',
+    },
+    {
+        id: 2,
+        title: 'Reprehend Lorem',
+        description: 'Quia molestiae reprehend…epturi deleniti ratione',
+        url: '#',
+        votes: 12,
+        avatar: './images/freepik-avatar-03.jpg',
+        submissionImage: './images/softwareD.png',
+    },
+    {
+        id: 3,
+        title: 'Lorem Voluptates',
+        description: 'Nihil ut voluptates blan…estias tempora quo quia',
+        url: '#',
+        votes: 14,
+        avatar: './images/freepik-avatar-04.jpg',
+        submissionImage: './images/taskman.png',
+    }
+];
